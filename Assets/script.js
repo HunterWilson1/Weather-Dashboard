8154d13c724b9db0208ff056084f45e99e41f05e
Hunter Wilson <huntertheguy123@gmail.com>
2023-02-02 14:24:23 -0600

    added an ajax to pull info from the api

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,12 @@
 const APIKey = "59e0d5247e1028ae9dbf1071b7d55e24"
 
-function grabWeather (queryURL) {
+function currentWeather (city) {
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?" + city + "&appid=" + APIKey;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(weatherResponse) {
+        console.log(weatherResponse)
+    })
 }
