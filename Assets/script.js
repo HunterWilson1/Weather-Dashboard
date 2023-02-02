916789ca68ad6399c97276a8a438a50c465fb483
Hunter Wilson <huntertheguy123@gmail.com>
2023-02-02 13:57:06 -0600

    I added a jquery link to use ajax to fetch data from api

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,5 @@
+const APIKey = "59e0d5247e1028ae9dbf1071b7d55e24"
 
-
-function grabWeather () {
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={59e0d5247e1028ae9dbf1071b7d55e24}"
+function grabWeather (queryURL) {
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?" + city + "&appid=" + APIKey;
 }
