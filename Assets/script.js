7a3b8476823368f664c11fa2905b4df5e2fc65b6
Hunter Wilson <huntertheguy123@gmail.com>
2023-02-06 13:54:36 -0600

    fixed the icon and now starting on the forecast

--- Assets/script.js
+++ Assets/script.js
@@ -4,7 +4,9 @@ let cityName = document.querySelector('#city');
 let temp = document.querySelector('#temp')
 let humidity = document.querySelector('#humidity');
 let windSpeed = document.querySelector('#wind-spd')
-let weatherIcon = document.querySelector('#weather')
+let weatherIcon = document.querySelector('#weatherIcon')
+let forecastContainer = document.querySelector('#forecast-container')
+
 
 
 function start() {
@@ -12,16 +14,16 @@ function start() {
 const APIKey = "59e0d5247e1028ae9dbf1071b7d55e24"
 
     function currentWeather (city) {
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-    let iconURL = "http://openweathermap.org/img/w/" + + ".png"
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
+    
         fetch(queryURL)
             .then((Response) => Response.json())
             .then((data) => {
            
-            console.log(data),
-            
+            console.log(data)
         
             cityName.innerText = citySearch.value
+            weatherIcon.setAttribute("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png")
             temp.innerText = "Temp: " + data.main.temp
             humidity.innerText = "Humidity: " + data.main.humidity
             windSpeed.innerText = "Wind Speed: " + data.wind.speed
@@ -30,10 +32,30 @@ const APIKey = "59e0d5247e1028ae9dbf1071b7d55e24"
 
     }
 
+    function getFiveDayForecast (cast) {
+      let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cast + "&units=imperial&appid=" + APIKey;
+        fetch(queryURL)
+         .then((Response) => Response.json())
+         .then((data) => {
+          console.log(data)
+
+          for (let i = 0; i < data.list.length; i+=8) {
+            console.log(data.list[i])
+          
+          var h2 = document.createElement("h2")
+          h2.innerText = data.city.name
+          forecastContainer.appendChild(h2)
+          }
+         }) 
+    }
+
+
+    
     searchBtn.addEventListener("click", 
     function(){
-        currentWeather(citySearch.value
-    )});
+        currentWeather(citySearch.value)
+        getFiveDayForecast(citySearch.value)
+      });
 }
 
 
